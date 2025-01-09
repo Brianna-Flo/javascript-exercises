7eba5d5da6fdd867150dcf1b29f4ce8570de2a7a
Brianna-Flo <145514791+Brianna-Flo@users.noreply.github.com>
2025-01-09 12:11:24 -0600

    completed exercises 01 to 04

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(wordToReverse) {
+    /*let wordAsArray = wordToReverse.split("");
+    wordAsArray.reverse();
+    const wordReversed = wordAsArray.join("");
+    return wordReversed;*/
+    return wordToReverse.split("").reverse().join("");
 };
 
 // Do not edit below this line
